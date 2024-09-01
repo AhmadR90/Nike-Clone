c161d80ebb58cc820563295679ab3873fc4d22d4
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-09-01 11:12:17 +0500

    half app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import PopularProducts from "./components/PopularProducts"
+import SuperQuality from "./components/SuperQuality"
+import Services from "./components/Services"
+import SpecialOffers from "./components/SpecialOffers"
+import CustomerReviews from "./components/CustomerReviews"
+import Subscribe from "./components/Subscribe"
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <main className="relative">
       <Navbar/>
-     <section className="xl:padding-1  wide:padding-r padding-b">
+     <section className="xl:padding-l  wide:padding-r padding-b">
       <Hero/>
 
      </section>
      <section className="padding">
-      Popular components
+      <PopularProducts/>
      </section>
      <section className="padding">
-      Super quality
+     <SuperQuality/>
      </section>
      <section className="padding-x py-10">
-      Services
+      <Services/>
      </section>
      <section className="padding">
-      Special offers
+      <SpecialOffers/>
      </section>
      <section className="padding bg-pale-blue" >
-      Customer Reviews
+      <CustomerReviews/>
      </section>
      <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
      </section>
      <section className="padding-x bg-black padding-t pb-8">
-      Footer
+      <Footer/>
      </section>
     </main>
   )
